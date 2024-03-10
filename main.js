54677592e3f3a177628e9699a4dddfeb3577fe6a
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-10 11:02:33 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Question no 19
+let guest_list1 = ['ali', 'asim', 'atif', 'rizwan'];
+let more_guest = ['shan', 'shahnwaz', 'sameer'];
+let full_guest = guest_list1.concat(more_guest);
+console.log(`I am inviting ${full_guest.length} people to dinner.`);
+while (full_guest.length > 2) {
+    let removed_guests = full_guest.pop(); //using while loop for reomoving guests 
+    console.log(`I'm sorry, ${removed_guests}, I can't invite you to dinner.`);
+}
+for (let newGuest of full_guest) {
+    console.log(`${newGuest}, you are still invited.`); // for loop using for who are reamaining guests
+}
